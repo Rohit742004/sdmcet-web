fc16fdfd23712aeacb0169791a2e9874dbb787db
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-05-23 16:31:00 +0530

    Create UniString_in_Php.php

---
+++ UniString_in_Php.php
@@ -0,0 +1,8 @@
+<?php
+    $Strcollege = "SDM College of Engineering";
+    $Strlen = strlen($Strcollege);
+    $Strpos = strpos($Strcollege, "E");
+
+    echo "Lenght of the String: ".$Strlen."<br/>";
+    echo "Position of the specified string is: ".$Strpos."<br/>";
+?>
