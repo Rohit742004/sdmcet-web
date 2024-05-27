fc50428d605a83b95c734fd49cfa9045a9d046f7
Rohit Badi <157134069+Rohit742004@users.noreply.github.com>
2024-05-27 18:45:39 +0530

    Create AdditionofTwoNumbers.php

---
+++ Unit-4/AdditionofTwoNumbers.php
@@ -0,0 +1,8 @@
+<?php
+    $num1 = $_POST["txt1"];
+    $num2 = $_POST["txt2"];
+
+    //Add two numbers
+    $result = $num1 + $num2;
+    echo "The Addition of $num1 and $num2 is <b>$result<b/>";
+?>
